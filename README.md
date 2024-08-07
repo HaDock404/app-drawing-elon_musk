969996131050b25d7140cf6f0efc89112edcb498
HaDock404 <delescluse.g@gmail.com>
2024-08-07 11:18:59 +0200

    YOLO push everything

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
-# Getting Started with Create React App
+# APP Elon Musk
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
 
-## Available Scripts
+Experience the art of CSS with a React app showcasing a detailed drawing of Elon Musk.   
 
-In the project directory, you can run:
+## Description  
 
-### `npm start`
+Welcome to Elon Musk CSS Portrait, a unique React application that demonstrates the potential of CSS art by recreating a detailed image of Elon Musk entirely through CSS. This project combines the power of React with advanced CSS techniques to showcase a stunning, pixel-perfect portrait, pushing the boundaries of what's possible with front-end development.  
+
+Here an exemple of the CSS Portrait.  
+
+![Example1](./documentation/example.png)
+
+## Table of Contents
+
+- [Getting Started with Create React App](#Getting-Started-with-Create-React-App)
+- [Launch Application](#Launch-Application)
+- [Contributing](#Contributing)
+- [License](#License)  
+
+## Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).  
+
+```bash
+git clone https://github.com/HaDock404/app-drawing-elon_musk.git
+cd app-drawing-elon_musk
+```  
+
+## Launch Application   
+
+```bash
+npm start
+```  
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
@@ -14,57 +40,13 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
+## Contributing
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
 
-### `npm run build`
+Please make sure to update tests as appropriate.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## License  
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
---
+++ src/components/Elon.jsx
@@ -0,0 +1,322 @@
+import '../styles/elon.css'
+
+function Elon() {
+    return (
+        <>
+    
+            <div class="right-hear1"></div>
+            <div class="right-hear2"></div>
+            <div class="right-hear3"></div>
+
+            <div class="right-eye1"></div>
+            <div class="right-eye2"></div>
+            <div class="right-eye3"></div>
+            <div class="right-eye4"></div>
+
+            <div class="left-eye1"></div>
+            <div class="left-eye2"></div>
+            <div class="left-eye3"></div>
+
+            <div class="right-cheek1"></div>
+            <div class="right-cheek2"></div>
+
+            <div class="left-chin1"></div>
+            <div class="left-chin2"></div>
+            
+            {/*<!--Yellow-->*/}
+
+            {/*<!--Orange-->*/}
+            
+            <div class="right-cheek3bis"></div>
+
+            <div class="middle-chin1"></div>
+            <div class="middle-chin2"></div>
+
+            <div class="mouth1"></div>
+            <div class="mouth2"></div>
+            <div class="mouth3"></div>
+            <div class="mouth4"></div>
+            <div class="mouth5"></div>
+            <div class="mouth6"></div>
+
+            <div class="right-cheek3"></div>
+            <div class="right-cheek4"></div>
+
+            <div class="left-cheek1"></div>
+            <div class="left-cheek2"></div>
+
+            <div class="nose1"></div>
+            <div class="nose2"></div>
+            <div class="nose3"></div>
+            <div class="nose4"></div>
+            <div class="nose5"></div>
+            <div class="nose6"></div>
+            <div class="nose7"></div>
+            <div class="nose8"></div>
+            <div class="nose9"></div>
+            <div class="nose10"></div>
+            <div class="nose11"></div>
+
+            <div class="right-eye5"></div>
+            <div class="right-eye6"></div>
+            <div class="right-eye7"></div>
+            <div class="right-eye8"></div>
+            <div class="right-eye9"></div>
+            <div class="right-eye10"></div>
+            <div class="right-eye11"></div>
+            <div class="right-eye12"></div>
+            <div class="right-eye13"></div>
+
+            <div class="left-eye4"></div>
+            <div class="left-eye5"></div>
+            <div class="left-eye6"></div>
+            <div class="left-eye7"></div>
+            <div class="left-eye8"></div>
+            <div class="left-eye9"></div>
+            <div class="left-eye10"></div>
+
+            <div class="hair6"></div>
+            
+            {/*<!--Orange-->*/}
+
+
+            {/*<!--Red-->*/}
+            
+            <div class="hair1"></div>
+            <div class="hair2"></div>
+            <div class="hair3"></div>
+            <div class="hair4"></div>
+            <div class="hair5"></div>
+
+            <div class="left-hear1"></div>
+            <div class="left-hear2"></div>
+            <div class="left-hear3"></div>
+            <div class="left-hear4"></div>
+
+            <div class="right-chin1"></div>
+            <div class="right-chin2"></div>
+            <div class="right-chin3"></div>
+            <div class="right-chin4"></div>
+            <div class="right-chin5"></div>
+
+            <div class="nose12"></div>
+            <div class="nose13"></div>
+            <div class="nose14"></div>
+
+            <div class="mouth7"></div>
+            <div class="mouth8"></div>
+            <div class="mouth9"></div>
+            <div class="mouth10"></div>
+            <div class="mouth11"></div>
+            <div class="mouth12"></div>
+            <div class="mouth13"></div>
+            <div class="mouth14"></div>
+            <div class="mouth15"></div>
+            <div class="mouth16"></div>
+            <div class="mouth17"></div>
+            <div class="mouth18"></div>
+            <div class="mouth19"></div>
+            <div class="mouth20"></div>
+
+            <div class="right-hear4"></div>
+            <div class="right-hear5"></div>
+            <div class="right-hear6"></div>
+            <div class="right-hear7"></div>
+
+            <div class="right-eye14"></div>
+            <div class="right-eye15"></div>
+            <div class="right-eye16"></div>
+            <div class="right-eye17"></div>
+            <div class="right-eye18"></div>
+            <div class="right-eye19"></div>
+            <div class="right-eye20"></div>
+            <div class="right-eye21"></div>
+            <div class="right-eye22"></div>
+            <div class="right-eye23"></div>
+            <div class="right-eye24"></div>
+            <div class="right-eye25"></div>
+            <div class="right-eye26"></div>
+            <div class="right-eye27"></div>
+
+            <div class="left-eye11"></div>
+            <div class="left-eye12"></div>
+            <div class="left-eye13"></div>
+            <div class="left-eye14"></div>
+            <div class="left-eye15"></div>
+            <div class="left-eye16"></div>
+            <div class="left-eye17"></div>
+            <div class="left-eye18"></div>
+            <div class="left-eye19"></div>
+            <div class="left-eye20"></div>
+            <div class="left-eye21"></div>
+            <div class="left-eye22"></div>
+            <div class="left-eye23"></div>
+            <div class="left-eye24"></div>
+            <div class="left-eye25"></div>
+            
+            {/*<!--Red-->*/}
+
+            {/*<!--Black-->*/}
+
+            <div class="hair7"></div>
+            <div class="hair8"></div>
+            <div class="hair9"></div>
+            <div class="hair10"></div>
+            <div class="hair11"></div>
+            <div class="hair12"></div>
+            <div class="hair13"></div>
+            <div class="hair14"></div>
+            <div class="hair15"></div>
+            <div class="hair16"></div>
+            <div class="hair17"></div>
+            <div class="hair18"></div>
+            <div class="hair19"></div>
+            <div class="hair20"></div>
+            <div class="hair21"></div>
+            <div class="hair22"></div>
+            <div class="hair23"></div>
+            <div class="hair24"></div>
+            <div class="hair25"></div>
+            <div class="hair26"></div>
+            <div class="hair27"></div>
+            <div class="hair28"></div>
+            <div class="hair29"></div>
+            <div class="hair30"></div>
+            <div class="hair31"></div>
+            <div class="hair32"></div>
+            <div class="hair33"></div>
+            <div class="hair34"></div>
+            <div class="hair35"></div>
+            <div class="hair36"></div>
+
+            <div class="left-hear5"></div>
+            <div class="left-hear6"></div>
+
+            <div class="right-hear8"></div>
+            <div class="right-hear9"></div>
+            <div class="right-hear10"></div>
+            <div class="right-hear11"></div>
+
+            <div class="chin1"></div>
+            <div class="chin2"></div>
+            <div class="chin3"></div>
+            <div class="chin4"></div>
+            <div class="chin5"></div>
+            <div class="chin6"></div>
+            <div class="chin7"></div>
+            <div class="chin8"></div>
+            <div class="chin9"></div>
+            <div class="chin10"></div>
+            <div class="chin11"></div>
+            <div class="chin12"></div>
+            <div class="chin13"></div>
+            <div class="chin14"></div>
+            <div class="chin15"></div>
+            <div class="chin16"></div>
+            <div class="chin17"></div>
+            <div class="chin18"></div>
+            <div class="chin19"></div>
+            <div class="chin20"></div>
+            <div class="chin21"></div>
+            <div class="chin22"></div>
+            <div class="chin23"></div>
+            <div class="chin24"></div>
+
+            <div class="mouth21"></div>
+            <div class="mouth22"></div>
+            <div class="mouth23"></div>
+            <div class="mouth24"></div>
+            <div class="mouth25"></div>
+            <div class="mouth26"></div>
+            <div class="mouth27"></div>
+            <div class="mouth28"></div>
+            <div class="mouth29"></div>
+            <div class="mouth30"></div>
+            <div class="mouth31"></div>
+            <div class="mouth32"></div>
+            <div class="mouth33"></div>
+            <div class="mouth34"></div>
+            <div class="mouth35"></div>
+            <div class="mouth36"></div>
+            <div class="mouth37"></div>
+            <div class="mouth38"></div>
+            <div class="mouth39"></div>
+            <div class="mouth40"></div>
+            <div class="mouth41"></div>
+            <div class="mouth42"></div>
+            <div class="mouth43"></div>
+            <div class="mouth44"></div>
+            <div class="mouth45"></div>
+
+            <div class="nose15"></div>
+            <div class="nose16"></div>
+            <div class="nose17"></div>
+            <div class="nose18"></div>
+            <div class="nose19"></div>
+            <div class="nose20"></div>
+            <div class="nose21"></div>
+            <div class="nose22"></div>
+            <div class="nose23"></div>
+            <div class="nose24"></div>
+            <div class="nose25"></div>
+            <div class="nose26"></div>
+            <div class="nose27"></div>
+            <div class="nose28"></div>
+            <div class="nose29"></div>
+
+            <div class="left-eye26"></div>
+            <div class="left-eye27"></div>
+            <div class="left-eye28"></div>
+            <div class="left-eye29"></div>
+            <div class="left-eye30"></div>
+            <div class="left-eye31"></div>
+            <div class="left-eye32"></div>
+            <div class="left-eye33"></div>
+            <div class="left-eye34"></div>
+            <div class="left-eye35"></div>
+            <div class="left-eye36"></div>
+            <div class="left-eye37"></div>
+            <div class="left-eye38"></div>
+            <div class="left-eye39"></div>
+            <div class="left-eye40"></div>
+            <div class="left-eye41"></div>
+            <div class="left-eye42"></div>
+            <div class="left-eye43"></div>
+            <div class="left-eye44"></div>
+            <div class="left-eye45"></div>
+            <div class="left-eye46"></div>
+            <div class="left-eye47"></div>
+            <div class="left-eye48"></div>
+
+            <div class="right-eye28"></div>
+            <div class="right-eye29"></div>
+            <div class="right-eye30"></div>
+            <div class="right-eye31"></div>
+            <div class="right-eye32"></div>
+            <div class="right-eye33"></div>
+            <div class="right-eye34"></div>
+            <div class="right-eye35"></div>
+            <div class="right-eye36"></div>
+            <div class="right-eye37"></div>
+            <div class="right-eye38"></div>
+            <div class="right-eye39"></div>
+            <div class="right-eye40"></div>
+            <div class="right-eye41"></div>
+            <div class="right-eye42"></div>
+            <div class="right-eye43"></div>
+            <div class="right-eye44"></div>
+            <div class="right-eye45"></div>
+            <div class="right-eye46"></div>
+            <div class="right-eye47"></div>
+            <div class="right-eye48"></div>
+            <div class="right-eye49"></div>
+            <div class="right-eye50"></div>
+            <div class="right-eye51"></div>
+            <div class="right-eye52"></div>
+
+            {/*<!--Black-->*/}
+        </>
+    )
+}
+
+export default Elon
